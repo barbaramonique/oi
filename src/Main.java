7dcc65100153da43098e3ce1cf9f890a95186e2d
unknown <barbara_setter@saojose.sc.senai.br>
2019-08-05 12:24:04 -0300

    oii

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Pessoas pessoa1 = new Pessoas("Barbs ", "Setter", 17, 175, feminino);
+		
+		
+	}
+	
+}
